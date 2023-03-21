d68475b5aef13471e797c0fad7866e85c24d3ade
Sandija-V <128511246+Sandija-V@users.noreply.github.com>
2023-03-21 19:23:24 +0200

    Add files via upload

---
+++ 1371a/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    //ifstream cin("a.in");
+    int t, n;
+    cin >> t;
+
+    for(int i=0; i<t; i++){
+        cin >> n;
+        cout << (n+1)/2 << endl;
+    }
+
+
+    return 0;
+}
