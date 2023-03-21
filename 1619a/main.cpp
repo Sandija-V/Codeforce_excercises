a041fc260266a7bb6ad22d5439532121dbc6a5de
Sandija-V <128511246+Sandija-V@users.noreply.github.com>
2023-03-21 19:23:46 +0200

    Add files via upload

---
+++ 1619a/main.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+
+bool isSquareString(string str, int len){
+    for(int i=0; i<len/2; i++)
+        if(str[i]!=str[i+len/2])
+            return false;
+
+    return true;
+
+}
+
+
+int main(){
+    //ifstream cin("a.in");
+
+    int t;
+    cin >> t;
+    string str;
+
+    for(int i=0; i<t; i++){
+        cin >> str;
+        int len = str.length();
+
+       /* if(len%2 != 0){
+            cout << "NO" << endl;
+        }else{
+            string str1(str.begin(), str.begin()+(len/2));
+            string str2(str.begin()+(len/2), str.end());
+            if(str1==str2){
+                cout << "YES" << endl;
+            }else{
+                cout << "NO" << endl;
+            }
+        }*/
+
+        if(len%2 != 0){
+            cout << "NO" << endl;
+        }else{
+            if(isSquareString(str, len)){
+                cout << "YES" << endl;
+            }else{
+                cout << "NO" << endl;
+            }
+        }
+    }
+
+    return 0;
+}
