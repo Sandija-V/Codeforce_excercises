8c5d2c6eb8738e45bd6f870f3b8208018ca68a92
Sandija-V <128511246+Sandija-V@users.noreply.github.com>
2023-03-27 22:33:58 +0300

    Add files via upload

---
+++ 510a/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int n, m;
+    cin >> n;
+    cin >> m;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+           /* if(i%4==0 && j==1){
+                cout << "#";
+            } else if(i%2==0 && j==m){
+                cout << "#";
+            } else if(i%2!=0){
+                cout << "#";
+            } else {
+                cout << ".";
+            }*/
+
+            if(i%2==0 || i%4==1 && j==m-1 || i%4==3 && j==0){
+                cout << "#";
+                } else {
+                    cout << ".";
+            }
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
